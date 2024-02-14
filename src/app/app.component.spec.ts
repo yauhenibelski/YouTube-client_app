a74185ff9f1b8e33e3f11539125f68b155340c5b
yauhenibelski <108809026+yauhenibelski@users.noreply.github.com>
2024-02-14 16:44:27 +0100

    chore: add prettier  eslint airbnb style guide

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -14,7 +14,7 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'YouTube-client_app' title`, () => {
+  it("should have the 'YouTube-client_app' title", () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
     expect(app.title).toEqual('YouTube-client_app');
